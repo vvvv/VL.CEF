c8c98d0dc0b06384d4536c7219dfa92a5ea401a7
Elias Holzer <elias@vvvv.org>
2021-11-30 18:18:08 +0100

    Updates VL to 2021.4 and version bump to 0.2.0
    - Skia renderer didn't work at all anymore due to Skia upgrade in 2021.4
    - Skia renderer now also using ANGLE - gets rid of WGLinterop and random access violations

--- src/VL.CEF.Core/SkiaRenderHandler.cs
+++ src/VL.CEF.Core/SkiaRenderHandler.cs
@@ -1,36 +1,37 @@
-﻿using OpenTK;
-using OpenTK.Graphics;
-using OpenTK.Graphics.OpenGL;
-using OpenTK.Platform.Windows;
-using SharpDX.Direct3D11;
+﻿using SharpDX.Direct3D11;
 using SkiaSharp;
-using Stride.Core.Mathematics;
 using System;
 using VL.Core;
-using VL.Lib.Basics.Resources;
 using VL.Skia;
 using Xilium.CefGlue;
 using Vector2 = Stride.Core.Mathematics.Vector2;
-using PixelFormat = SharpDX.DXGI.Format;
-using All = OpenTK.Graphics.OpenGL.All;
 using System.Collections.Generic;
 using System.Linq;
+using VL.Skia.Egl;
+using SharpDX.DXGI;
+using Device = SharpDX.Direct3D11.Device;
+using System.Threading;
 
 namespace VL.CEF
 {
     public sealed partial class SkiaRenderHandler : IRenderHandler, ILayer
     {
         private readonly object syncRoot = new object();
-        private readonly IResourceHandle<Device> deviceHandle;
-        private IntPtr wglDeviceHandle;
-        private IntPtr sharedSurfaceHandle;
-        private SKImage surface;
+
+        private readonly RenderContext renderContext;
+        private readonly Device device;
+        private SKImage rasterImage, textureImage;
+        private Texture2D sharedTexture;
+        private IntPtr sharedHandle;
         private WebRenderer webRenderer;
 
         public SkiaRenderHandler(NodeContext nodeContext)
         {
-            var deviceProvider = nodeContext.Factory.CreateService<IResourceProvider<Device>>(nodeContext);
-            deviceHandle = deviceProvider?.GetHandle();
+            renderContext = RenderContext.ForCurrentThread();
+            if (renderContext.EglContext.Dislpay.TryGetD3D11Device(out var d3dDevice))
+            {
+                device = new Device(d3dDevice);
+            }
         }
 
         public void Initialize(WebRenderer webRenderer)
@@ -40,12 +41,10 @@ namespace VL.CEF
 
         public void Dispose()
         {
-            surface?.Dispose();
-            foreach (var sharedTexture in sharedTextures.Values)
-                sharedTexture?.Dispose();
-            if (wglDeviceHandle != default)
-                Wgl.DXCloseDeviceNV(wglDeviceHandle);
-            deviceHandle?.Dispose();
+            rasterImage?.Dispose();
+            textureImage?.Dispose();
+            sharedTexture?.Dispose();
+            renderContext.Dispose();
         }
 
         public void OnPaint(CefPaintElementType type, CefRectangle[] cefRects, IntPtr buffer, int width, int height)
@@ -53,24 +52,26 @@ namespace VL.CEF
             var image = SKImage.FromPixelCopy(new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul, SKColorSpace.CreateSrgb()), buffer, width * 4);
             lock (syncRoot)
             {
-                surface?.Dispose();
-                surface = image;
+                rasterImage?.Dispose();
+                rasterImage = image;
             }
         }
 
         public void OnAcceleratedPaint(CefPaintElementType type, CefRectangle[] dirtyRects, IntPtr sharedHandle)
         {
-            lock (this)
+            lock (syncRoot)
             {
-                sharedSurfaceHandle = sharedHandle;
+                if (sharedHandle != this.sharedHandle)
+                {
+                    this.sharedHandle = sharedHandle;
+                    sharedTexture?.Dispose();
+                    sharedTexture = device?.OpenSharedResource<Texture2D>(sharedHandle);
+                }
             }
         }
 
         public void Render(CallerInfo caller)
         {
-            if (caller.GRContext is null)
-                return;
-
             var canvas = caller.Canvas;
             var localClipBounds = canvas.LocalClipBounds;
             var deviceClipBounds = canvas.DeviceClipBounds;
@@ -78,188 +79,104 @@ namespace VL.CEF
             // Ensure we render in the proper size
             webRenderer.Size = new Vector2(deviceClipBounds.Width, deviceClipBounds.Height);
 
-            if (surface != null)
+            lock (syncRoot)
             {
-                lock (syncRoot)
+                // Transfer ownership
+                var texture = Interlocked.Exchange(ref sharedTexture, null);
+                if (texture != null)
                 {
-                    if (surface != null)
-                    {
-                        canvas.DrawImage(surface, localClipBounds);
-                    }
-                }
-            }
-            else
-            {
-                var sharedHandle = sharedSurfaceHandle;
-                if (sharedHandle == IntPtr.Zero)
-                    return;
-
-                var device = deviceHandle.Resource;
-                if (device is null)
-                    return;
-
-                if (wglDeviceHandle == IntPtr.Zero)
-                {
-                    // We assume that an OpenGL context is current
-                    wglDeviceHandle = Wgl.DXOpenDeviceNV(device.NativePointer);
-                    if (wglDeviceHandle == IntPtr.Zero)
-                        return;
+                    textureImage?.Dispose();
+                    textureImage = ToImage(texture);
                 }
 
-                lock (this)
-                {
-                    if (!sharedTextures.TryGetValue(sharedHandle, out var sharedTexture))
-                    {
-                        var texture = device.OpenSharedResource<Texture2D>(sharedHandle);
-                        foreach (var entry in sharedTextures.ToList())
-                        {
-                            var description = entry.Value.Texture.Description;
-                            if (description.Width != texture.Description.Width || description.Height != texture.Description.Height || description.Format != texture.Description.Format)
-                            {
-                                sharedTextures.Remove(entry.Key);
-                                entry.Value.Dispose();
-                            }
-                        }
-                        sharedTexture = SharedTextureImage.Create(caller.GRContext, wglDeviceHandle, texture);
-                        sharedTextures.Add(sharedHandle, sharedTexture);
-                    }
-
-                    if (sharedTexture != null && sharedTexture.Lock())
-                    {
-                        canvas.DrawImage(sharedTexture.Image, localClipBounds);
-                        canvas.Flush();
-                        sharedTexture.Unlock();
-                    }
-                }
+                var image = textureImage ?? rasterImage;
+                if (image != null)
+                    canvas.DrawImage(image, localClipBounds);
             }
         }
-        private readonly Dictionary<IntPtr, SharedTextureImage> sharedTextures = new Dictionary<IntPtr, SharedTextureImage>();
 
-        sealed class SharedTextureImage : IDisposable
+        SKImage ToImage(Texture2D texture)
+        {    
+            // Taken form VL.Video.MediaFoundation
+            const int GL_TEXTURE_BINDING_2D = 0x8069;
+
+            var eglContext = renderContext.EglContext;
+            var eglImage = eglContext.CreateImageFromD3D11Texture(texture.NativePointer);
+
+            uint textureId = 0;
+            NativeGles.glGenTextures(1, ref textureId);
+
+            // We need to restore the currently bound texture (https://github.com/devvvvs/vvvv/issues/5925)
+            NativeGles.glGetIntegerv(GL_TEXTURE_BINDING_2D, out var currentTextureId);
+            NativeGles.glBindTexture(NativeGles.GL_TEXTURE_2D, textureId);
+            NativeGles.glEGLImageTargetTexture2DOES(NativeGles.GL_TEXTURE_2D, eglImage);
+            NativeGles.glBindTexture(NativeGles.GL_TEXTURE_2D, (uint)currentTextureId);
+
+            var description = texture.Description;
+            var colorType = GetColorType(description.Format);
+            var glInfo = new GRGlTextureInfo(
+                id: textureId,
+                target: NativeGles.GL_TEXTURE_2D,
+                format: colorType.ToGlSizedFormat());
+
+            var backendTexture = new GRBackendTexture(
+                width: description.Width,
+                height: description.Height,
+                mipmapped: false,
+                glInfo: glInfo);
+
+            var image = SKImage.FromTexture(
+                renderContext.SkiaContext,
+                backendTexture,
+                GRSurfaceOrigin.BottomLeft,
+                colorType,
+                SKAlphaType.Premul,
+                colorspace: SKColorSpace.CreateSrgb(),
+                releaseProc: _ =>
+                {
+                    renderContext.MakeCurrent();
+
+                    backendTexture.Dispose();
+                    NativeGles.glDeleteTextures(1, ref textureId);
+                    eglImage.Dispose();
+                    texture.Dispose();
+                });
+
+            return image;
+        }
+
+        static SKColorType GetColorType(Format format)
         {
-            public static SharedTextureImage Create(GRContext context, IntPtr deviceHandle, Texture2D texture)
+            switch (format)
             {
-                var name = (uint)GL.GenTexture();
-                var textureHandle = Wgl.DXRegisterObjectNV(
-                    deviceHandle,
-                    texture.NativePointer,
-                    name,
-                    (uint)All.Texture2D,
-                    WGL_NV_DX_interop.AccessReadOnly);
-
-                if (textureHandle == default)
-                {
-                    Wgl.DXUnregisterObjectNV(deviceHandle, textureHandle);
-                    GL.DeleteTexture(name);
-                    return null;
-                }
-
-                var pixelFormat = PixelFormat.R8G8B8A8_UNorm;
-                var pixelConfig = GetPixelConfig(pixelFormat);
-                var colorType = pixelConfig.ToColorType();
-                var backendTexture = new GRBackendTexture(
-                    texture.Description.Width,
-                    texture.Description.Height,
-                    mipmapped: false,
-                    new GRGlTextureInfo((uint)All.Texture2D, name, pixelConfig.ToGlSizedFormat()));
-
-                var image = SKImage.FromTexture(
-                    context,
-                    backendTexture,
-                    GRSurfaceOrigin.BottomLeft,
-                    colorType,
-                    SKAlphaType.Premul,
-                    colorspace: null /*SKColorSpace.CreateSrgb()*/,
-                    releaseProc: _ =>
-                    {
-                    });
-
-                if (image is null)
-                {
-                    Wgl.DXUnregisterObjectNV(deviceHandle, textureHandle);
-                    GL.DeleteTexture(name);
-                    return null;
-                }
-
-                return new SharedTextureImage(image, texture, textureHandle, deviceHandle, name);
-            }
-
-            static GRPixelConfig GetPixelConfig(PixelFormat format)
-            {
-                switch (format)
-                {
-                    case PixelFormat.B8G8R8A8_UNorm:
-                        return GRPixelConfig.Bgra8888;
-                    case PixelFormat.B8G8R8A8_UNorm_SRgb:
-                        return GRPixelConfig.Sbgra8888;
-                    case PixelFormat.R8G8B8A8_UNorm:
-                        return GRPixelConfig.Rgba8888;
-                    case PixelFormat.R8G8B8A8_UNorm_SRgb:
-                        return GRPixelConfig.Srgba8888;
-                    case PixelFormat.R10G10B10A2_UNorm:
-                        return GRPixelConfig.Rgba1010102;
-                    case PixelFormat.R16G16B16A16_Float:
-                        return GRPixelConfig.RgbaHalf;
-                    case PixelFormat.R32G32B32A32_Float:
-                        return GRPixelConfig.RgbaFloat;
-                    case PixelFormat.R32G32_Float:
-                        return GRPixelConfig.RgFloat;
-                    case PixelFormat.A8_UNorm:
-                        return GRPixelConfig.Alpha8;
-                    case PixelFormat.R8_UNorm:
-                        return GRPixelConfig.Gray8;
-                    case PixelFormat.B5G6R5_UNorm:
-                        return GRPixelConfig.Rgb565;
-                    default:
-                        return GRPixelConfig.Unknown;
-                }
-            }
-
-            public readonly SKImage Image;
-            public readonly Texture2D Texture;
-            public readonly IntPtr TextureHandle;
-            public readonly IntPtr DeviceHandle;
-            public readonly uint Name;
-
-            public SharedTextureImage(SKImage image, Texture2D texture, IntPtr textureHandle, IntPtr deviceHandle, uint name)
-            {
-                Image = image;
-                Texture = texture;
-                TextureHandle = textureHandle;
-                DeviceHandle = deviceHandle;
-                Name = name;
-            }
-
-            public bool Lock()
-            {
-                unsafe
-                {
-                    void** objects = stackalloc void*[1];
-
-                    objects[0] = TextureHandle.ToPointer();
-
-                    return Wgl.DXLockObjectsNV(DeviceHandle, 1, new IntPtr(objects));
-                }
-            }
-
-            public bool Unlock()
-            {
-                unsafe
-                {
-                    void** objects = stackalloc void*[1];
-
-                    objects[0] = TextureHandle.ToPointer();
-
-                    return Wgl.DXUnlockObjectsNV(DeviceHandle, 1, new IntPtr(objects));
-                }
-            }
-
-            public void Dispose()
-            {
-                Image.Dispose();
-                Wgl.DXUnregisterObjectNV(DeviceHandle, TextureHandle);
-                GL.DeleteTexture(Name);
-                Texture.Dispose();
+                case Format.B5G6R5_UNorm:
+                    return SKColorType.Rgb565;
+                case Format.B8G8R8A8_UNorm:
+                case Format.B8G8R8A8_UNorm_SRgb:
+                    return SKColorType.Bgra8888;
+                case Format.R8G8B8A8_UNorm:
+                case Format.R8G8B8A8_UNorm_SRgb:
+                    return SKColorType.Rgba8888;
+                case Format.R10G10B10A2_UNorm:
+                    return SKColorType.Rgba1010102;
+                case Format.R16G16B16A16_Float:
+                    return SKColorType.RgbaF16;
+                case Format.R16G16B16A16_UNorm:
+                    return SKColorType.Rgba16161616;
+                case Format.R32G32B32A32_Float:
+                    return SKColorType.RgbaF32;
+                case Format.R16G16_Float:
+                    return SKColorType.RgF16;
+                case Format.R16G16_UNorm:
+                    return SKColorType.Rg1616;
+                case Format.R8G8_UNorm:
+                    return SKColorType.Rg88;
+                case Format.A8_UNorm:
+                    return SKColorType.Alpha8;
+                case Format.R8_UNorm:
+                    return SKColorType.Gray8;
+                default:
+                    return SKColorType.Unknown;
             }
         }
     }
